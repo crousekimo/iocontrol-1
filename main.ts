8ddaaa9565d5329d5125e48053c03a532a2d4b24
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-12-06 11:41:52 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+//% weight=0 color=#EC7505 icon="\uf0ad" block="iocontrolv1"
+namespace iocontrolv1 {
+
+    //% blockId=analogRead block="analogRead Pin %pin value"
+    //% weight=50
+    //% blockExternalInputs = 1
+    export function analog(pin: number):number {
+	let a = pins.analogReadPin(pin+100)
+	return a
+    }
+
+    //% blockId=analogRead block="digitalRead Pin %pin value"
+    //% weight=50
+    //% blockExternalInputs = 1
+    export function analogRead(pin: number):number {
+	let a=pins.digitalReadPin(pin+100)
+	return a
+    }
+
+    //% blockId=digitalWrite block="digitalWrite Pin %pin value %value"
+    //% weight=50
+    //% blockExternalInputs = 1
+    export function digitalWrite(pin: number,value: number):void {
+      pins.digitalWritePin(pin+100, value)
+    }
+
+    //% blockId=analogWrite block="analogWrite Pin %pin value %value"
+    //% weight=50
+    //% blockExternalInputs = 1
+    export function analogWrite(pin: number,value: number):void {
+      pins.digitalWritePin(pin+100, value)
+    }
+
+}
